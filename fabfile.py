d7e5673345c7aadc6b17e5ed63df27d780c9e0eb
Mirko Friedenhagen <mfriedenhagen@gmail.com>
2014-08-02 10:26:22 +0200

    Extract a class.

--- fabfile.py
+++ fabfile.py
@@ -56,18 +56,63 @@ def jenkins_update_center():
         abort("updates upload not ok {}".format(reply.text))
     logging.info('applied updates json')
 
+class JenkinsUpdatePlugins(object):
 
-def convert_updated_plugins_to_xml(plugins):
-    plugins_xml = [u"<root>"]
-    for plugin in plugins:
-        name = plugin["name"]
-        version = plugin["version"]
-        installed = plugin["installed"]["version"]
-        plugins_xml.append(u"""<{name} plugin="{name}@{version}"><installed plugin="{name}@{installed}"/></{name}>""".format
-                           (name=name, version=version, installed=installed))
-    plugins_xml.append(u"</root>")
-    return u"".join(plugins_xml)
+    def __init__(self, root_url, user, password):
+        self.root_url = root_url
+        self.auth = (user, password)
 
+    @staticmethod
+    def convert_updated_plugins_to_xml(plugins):
+        plugins_xml = [u"<root>"]
+        for plugin in plugins:
+            name = plugin["name"]
+            version = plugin["version"]
+            installed = plugin["installed"]["version"]
+            plugins_xml.append(u"""<update plugin="{name}@{version}"><installed plugin="{name}@{installed}"/></update>""".format
+                               (name=name, version=version, installed=installed))
+        plugins_xml.append(u"</root>")
+        return u"".join(plugins_xml)
+
+    def get_updated_plugins_metadata(self):
+        logging.info("get_updated_plugins_metadata")
+        reply = self._get(
+            "updateCenter/site/default/api/json?pretty=true&depth=2&tree=updates[name,version,installed[shortName,version]]")
+        if not reply.ok:
+            abort("Could not get updates from updateCenter")
+        plugins_json = reply.content
+        plugins = json.loads(plugins_json)["updates"]
+        logging.debug("outdated_plugins: {}".format(plugins))
+        return plugins
+
+    def prevalidate_configuration(self, plugins_xml):
+        reply = self._post("pluginManager/prevalidateConfig", plugins_xml)
+        if not reply.ok:
+            abort("Could not prevalidateConfig {}".format(reply.content))
+        logging.info("Outdated plugins: {}".format(json.loads(reply.content)))
+
+    def install_necessary_plugins(self, plugins_xml):
+        reply = self._post("pluginManager/installNecessaryPlugins", plugins_xml)
+        if not reply.ok:
+            abort("Could not install plugins {}".format(reply.content))
+        logging.info("pluginManager/installNecessaryPlugins: {}".format(reply.status_code))
+
+    def wait_for_installation_of_plugins(self):
+        reply = self._get("updateCenter/api/json?depth=1")
+        if not reply.ok:
+            abort("")
+        installation_jobs = [job for job in json.loads(reply.content)["jobs"] if job["type"] == "InstallationJob"]
+        print installation_jobs
+
+    def _post(self, path, data):
+        return requests.post(
+            self.root_url +
+            path, data=data, auth=self.auth)
+
+    def _get(self, path):
+        return requests.get(
+            self.root_url +
+            path, auth=self.auth)
 
 @task
 def jenkins_update_plugins():
@@ -77,27 +122,18 @@ def jenkins_update_plugins():
     require("jenkins_user")
     require("jenkins_token")
 
-    auth = (env.jenkins_user, env.jenkins_token)
+
+
     #execute(jenkins_update_center)
-    reply = requests.get(
-        HUSCHTEGUZZEL_URL +
-        "updateCenter/site/default/api/json?pretty=true&depth=2&tree=updates[name,version,installed[shortName,version]]", auth=auth)
-    if not reply.ok:
-        abort("Could not get api")
-    plugins_json = reply.content
-    plugins = json.loads(plugins_json)["updates"]
-    logging.info("outdated_plugins: {}".format(plugins))
-    plugins_xml = convert_updated_plugins_to_xml(plugins)
+    j = JenkinsUpdatePlugins(HUSCHTEGUZZEL_URL, env.jenkins_user, env.jenkins_token)
+    plugins = j.get_updated_plugins_metadata()
+    plugins_xml = j.convert_updated_plugins_to_xml(plugins)
     logging.info("plugins_xml: {}".format(plugins_xml))
-    reply = requests.post(HUSCHTEGUZZEL_URL + "pluginManager/prevalidateConfig", data=plugins_xml, auth=auth)
-    if not reply.ok:
-        abort("Could not prevalidateConfig {}".format(reply.content))
-    logging.info("Outdated plugins: {}".format(json.loads(reply.content)))
-    return
-    reply = requests.post(HUSCHTEGUZZEL_URL + "pluginManager/installNecessaryPlugins", data=plugins_xml, auth=auth)
-    if not reply.ok:
-        abort("Could not install plugins {}".format(reply.content))
-    logging.info("pluginManager/installNecessaryPlugins: {}".format(reply.status_code))
+    j.prevalidate_configuration(plugins_xml)
+    j.install_necessary_plugins(plugins_xml)
+    j.wait_for_installation_of_plugins()
+
+    print("Ready for restart")
 
 
 @task
