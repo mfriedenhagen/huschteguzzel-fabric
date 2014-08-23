65c11e92cb0b41164cb79e1050810691e20a9bd5
Mirko Friedenhagen <mfriedenhagen@gmail.com>
2014-08-23 04:10:05 +0200

    Add nginx and triggers.

--- bundles/mybundle/bundle.py
+++ bundles/mybundle/bundle.py
@@ -10,5 +10,39 @@ files = {
         'group': 'root',
         'mode': '0644',
         'source': "etc/default/jenkins",
+        'triggers': [
+            "action:restart_jenkins",
+        ]
     },
+    '/etc/nginx/sites-available/default': {
+        'owner': 'root',
+        'group': 'root',
+        'mode': '0644',
+        'source': 'etc/nginx/sites-available/default',
+        'triggers': [
+            "action:reload_nginx"
+        ]
+    },
+}
+
+symlinks = {
+    "/etc/nginx/sites-enabled/default": {
+        "group": "root",
+        "owner": "root",
+        "target": "/etc/nginx/sites-available/default",
+        "needs": [
+            "file:/etc/nginx/sites-available/default"
+        ]
+    },
+}
+
+actions = {
+    "restart_jenkins": {
+        "command": "service jenkins restart",
+        "triggered": True,
+    },
+    "reload_nginx": {
+        "command": "service nginx reload",
+        "triggered": True
+    }
 }
