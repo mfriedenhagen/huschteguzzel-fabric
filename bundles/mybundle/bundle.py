6c20a8a456396d259caf973f9d63290007f5f030
Mirko Friedenhagen <mfriedenhagen@gmail.com>
2014-08-23 03:38:36 +0200

    Fix permissions, add jenkins default

--- bundles/mybundle/bundle.py
+++ bundles/mybundle/bundle.py
@@ -1,5 +1,14 @@
 files = {
     '/etc/motd': {
+        'owner': 'root',
+        'group': 'root',
+        'mode': '0644',
         'source': "etc/motd",
+    },
+    '/etc/default/jenkins': {
+        'owner': 'root',
+        'group': 'root',
+        'mode': '0644',
+        'source': "etc/default/jenkins",
     },
 }
