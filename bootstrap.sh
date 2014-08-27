89924842e937fdebe22087f4d6f7cca661bfeb96
Mirko Friedenhagen <mirko.friedenhagen@1und1.de>
2014-08-27 18:52:21 +0200

    Alternate venv dir

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,15 @@
-#!/bin/sh
-venv=`basename $PWD`
-virtualenv -p /usr/bin/python2.7 .venv/$venv
-. .venv/${venv}/bin/activate
-pip -q install -r requirements.txt
+#!/bin/bash
+VENV_BASE=${VENV_BASE:-.venv}
+VENV_NAME=$(basename $PWD)
+VENV_DIR=${VENV_BASE}/${VENV_NAME}
+virtualenv -p /usr/bin/python2.7 $VENV_DIR
+. $VENV_DIR/bin/activate
+if [ -r /etc/mam_proxy.sh ]
+then
+    . /etc/mam_proxy.sh
+    export http_proxy=$ui_http_proxy
+    export https_proxy=$ui_http_proxy
+    pip install -r requirements.txt
+else
+    pip install -r requirements.txt
+fi
