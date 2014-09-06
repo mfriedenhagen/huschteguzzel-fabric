a189f4594da83f672162b20c944844b498b5fbad
Mirko Friedenhagen <mfriedenhagen@gmail.com>
2014-09-06 22:08:46 +0200

    Make an all group.

--- groups.py
+++ groups.py
@@ -1,15 +1,4 @@
 groups = {
-    #'group1': {
-    #    'bundles': (
-    #        'bundle1',
-    #    ),
-    #    'members': (
-    #        'node1',
-    #    ),
-    #    'subgroups': (
-    #        'group2',
-    #    ),
-    #},
     'all': {
         'member_patterns': (
             r".*",
