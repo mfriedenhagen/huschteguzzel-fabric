09041d3a25b8bf7797e5576402235a39546e889c
Mirko Friedenhagen <mfriedenhagen@gmail.com>
2014-09-07 22:44:42 +0200

    Add jdk and maven settings.

--- bundles/jenkins/bundle.py
+++ bundles/jenkins/bundle.py
@@ -1,22 +1,48 @@
+pkg_apt = {
+    "jenkins": { "installed": True },
+    "oracle-java8-installer": { "installed": True },
+    "oracle-java7-installer": { "installed": True },
+}
+
 files = {
-    '/etc/default/jenkins': {
-        'owner': 'root',
-        'group': 'root',
-        'mode': '0644',
-        'source': "etc/default/jenkins",
-        'triggers': [
+    "/etc/default/jenkins": {
+        "owner": "root",
+        "group": "root",
+        "mode": "0644",
+        "source": "etc/default/jenkins",
+        "triggers": [
             "svc_systemv:jenkins:restart",
         ]
     },
+    "/var/lib/jenkins/.m2/settings.xml": {
+        "owner": "jenkins",
+        "group": "adm",
+        "mode": "0640",
+        "content_type": "text",
+        "source": "m2/settings.xml",
+    },
+    "/var/lib/jenkins/.m2/toolchains.xml": {
+        "owner": "jenkins",
+        "group": "adm",
+        "mode": "0640",
+        "content_type": "text",
+        "source": "m2/toolchains.xml",
+    }
 }
 
-jenkins = {
-    "huschteguzzel": {
-        "jenkins_url": "https://huschteguzzel.de/hudson/",
+directories = {
+    "/var/lib/jenkins/.m2": {
+        "owner": "jenkins",
+        "group": "adm",
+        "mode": "0750",
+        "needed_by": [
+            "file:/var/lib/jenkins/.m2/toolchains.xml",
+            "file:/var/lib/jenkins/.m2/settings.xml"
+        ]
     }
 }
 
 svc_systemv = {
-    'jenkins': {'running': True},
+    "jenkins": { "running": True },
 }
 
