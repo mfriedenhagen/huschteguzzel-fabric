e8cab185c0d9ba4dd318c100575ece2d08db45c2
Mirko Friedenhagen <mfriedenhagen@gmail.com>
2014-09-19 22:57:07 +0200

    Move apt-sources creation to library.

--- bundles/apt/bundle.py
+++ bundles/apt/bundle.py
@@ -1,15 +1,6 @@
 files = {}
-_sources_list_d_prefix = "etc/apt/sources.list.d/"
-for f in ("jenkins.list", "webupd8team-java-precise.list",):
-    files["/{}{}".format(_sources_list_d_prefix, f)] = {
-        'owner': 'root',
-        'group': 'root',
-        'mode': '0644',
-        'source': "{}{}".format(_sources_list_d_prefix, f)
-    }
 
-del(f)
-del(_sources_list_d_prefix)
+files.update(repo.libs.utils.apt_sources("jenkins.list", "webupd8team-java-precise.list"))
 
 actions = {
     'apt_update': {
