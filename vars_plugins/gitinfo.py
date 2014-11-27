c78e076b46a037bbf73919c2e33d46ab105900db
Mirko Friedenhagen <mfriedenhagen@gmail.com>
2014-11-27 21:48:26 +0100

    Include custom facts.

--- vars_plugins/gitinfo.py
+++ vars_plugins/gitinfo.py
@@ -15,7 +15,7 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with Ansible.  If not, see <http://www.gnu.org/licenses/>.
-
+import getpass
 import subprocess
 
 class VarsModule(object):
@@ -25,34 +25,19 @@ class VarsModule(object):
     """
 
     def __init__(self, inventory):
-
         """ constructor """
-
         self.inventory = inventory
         self.inventory_basedir = inventory.basedir()
 
 
-    def run(self, host, vault_password=None):
-        """ For backwards compatibility, when only vars per host were retrieved
-            This method should return both host specific vars as well as vars
-            calculated from groups it is a member of """
-        return {}
-
-
     def get_host_vars(self, host, vault_password=None):
         """ Get host specific variables. """
-        if VarsModule.revision is None:
-            VarsModule.revision = subprocess.check_output(("git", "rev-parse", "--short", "--porcelain", "HEAD")).strip()
-            VarsModule.branch = [branch for branch in subprocess.check_output(("git", "branch")).strip().split("\n") if branch.startswith("*")][0][2:]
-            VarsModule.modified = subprocess.check_output(("git", "status", "--porcelain")).strip() and "MODIFIED" or "UNMODIFIED"
-        host.set_variable('gitinfo_revision', VarsModule.revision)
-        host.set_variable('gitinfo_branch', VarsModule.branch)
-        host.set_variable('gitinfo_modified', VarsModule.modified)
-
-
-    def get_group_vars(self, group, vault_password=None):
-        """ Get group specific variables. """
-        return {}
-VarsModule.revision = None
-VarsModule.branch = None
-VarsModule.modified = None
+        if VarsModule.info is None:
+            info = {}
+            info["revision"] = subprocess.check_output(("git", "rev-parse", "--short", "--porcelain", "HEAD")).strip()
+            info["branch"] = [branch for branch in subprocess.check_output(("git", "branch")).strip().split("\n") if branch.startswith("*")][0][2:]
+            info["modified"] = subprocess.check_output(("git", "status", "--porcelain")).strip() and "MODIFIED" or "UNMODIFIED"
+            info["user"] = getpass.getuser()
+            VarsModule.info = info
+        host.set_variable('gitinfo', VarsModule.info)
+VarsModule.info = None
